49d954cab6d681564b6550845f75f56b8e4502b2
francescodelduchetto <francescodelduchetto@gmail.com>
2020-03-09 09:33:41 +0000

    download model from lcas.ownclod

--- download_model.sh
+++ download_model.sh
@@ -1,13 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+mkdir -p models
 
-mkdir models
+wget -O models/lstm_10_50_runsigm_runsigm.h5 'http://lcas.lincoln.ac.uk/owncloud/index.php/s/UHWU1g5qXU9RiAp/download'
 
-export fileid='1rDOc24g7RB_1U0S7dBU1_MPZDyB629Md'
-export filename='models/lstm_10_50_runsigm_runsigm.h5'
-
-wget --save-cookies cookies.txt 'https://docs.google.com/uc?export=download&id='$fileid -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1/p' > confirm.txt
-
-wget --load-cookies cookies.txt -O $filename 'https://docs.google.com/uc?export=download&id='$fileid'&confirm='$(<confirm.txt) 
-
-
-rm cookies.txt confirm.txt
