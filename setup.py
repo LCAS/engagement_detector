b635f130697d9fff214c724e53bd488b12675f3d
Marc Hanheide <marc@hanheide.net>
2024-02-19 17:30:26 +0000

    setup py for ROS2 (and correct TF)

--- setup.py
+++ setup.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python
 
 from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import find_packages
 
-d = generate_distutils_setup(
-    packages=['engagement_detector'],
-    # scripts=['scripts'],
+package_name = 'engagement_detector'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=['numpy', 'pillow', 'tensorflow-gpu', 'keras', 'opencv-python', 'rospkg']
+    install_requires=['numpy', 'pillow', 'tensorflow', 'keras', 'opencv-python', 'rospkg'],
+    zip_safe=True,
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache License 2.0',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Engagement Detector package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'engagement_detector_node = engagement_detector.ros_engagement_detector:main'
+        ],
+    },
 )
-
-setup(**d)
