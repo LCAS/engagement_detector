7c3b723a836135b928d5b850b5047670eaaf9e0c
Marc Hanheide <marc@hanheide.net>
2024-02-19 17:43:07 +0000

    fix ROS2 compatibility

--- setup.py
+++ setup.py
@@ -8,9 +8,13 @@ package_name = 'engagement_detector'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    install_requires=['numpy', 'pillow', 'tensorflow', 'keras', 'opencv-python', 'rospkg'],
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
     zip_safe=True,
     keywords=['ROS'],
     classifiers=[
